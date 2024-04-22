9b1ad5a5e31e7d3d423e86772ede20d7b91c52be
Rushil Patel <rushil.patel791@gmail.com>
2024-04-22 19:25:40 +0100

    chore: db types

---
+++ types/swap.ts
@@ -0,0 +1,4 @@
+export interface Swap {
+    oldplayerid: number;
+    newplayerid: number;
+}
---
+++ types/team.ts
@@ -0,0 +1,5 @@
+export interface Team {
+    fullname: string;
+    teamname: string;
+    total: number;
+}
