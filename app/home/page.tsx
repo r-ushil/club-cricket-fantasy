ea311c190b8875c34c676fea706bc372353d8af8
Rushil Patel <rushil.patel791@gmail.com>
2024-04-22 19:26:22 +0100

    feat: skeleton home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,127 @@
-import NavBar from "@/components/NavBar";
+import { createClient } from "@/utils/supabase/server";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+import { Team } from "@/types/team";
+import { Swap } from "@/types/swap";
+import { Player } from "@/types/player";
+import TeamDisplay from "@/components/TeamDisplay";
+import Leaderboard from "@/components/Leaderboard";
 
-const HomePage = () => {
+const getSupabaseInfo = async () => {
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+  if (!user) {
+    return redirect("/login");
+  } else {
+    const players: Player[] = await getTeamPlayers(supabase, user.id);
+    const swaps: Swap[] = await getSwaps(supabase, user.id);
+    const [userTeamInfo, teams]: [Team, Team[]] = await getTeams(supabase, user.id);
+
+    const currentGWPoints = await getCurrentGWPoints(supabase, players);
+
+    
+    return { players, userTeamInfo, teams, swaps , currentGWPoints};
+  }
+
+}
+
+const getCurrentGWPoints = async (supabase: SupabaseClient<any, "public", any>, players: Player[]): Promise<number> => {
+
+  const { data: playersObjs } = await supabase.from("players").select("currentgw").in("playerid", players.map(player => player.playerid));
+
+  if (!playersObjs || playersObjs.length === 0) {
+    return 0;
+  }
+
+  const points = playersObjs.reduce((acc, player) => acc + player.currentgw, 0);
+
+  return points;
+
+}
+
+const getTeamPlayers = async (supabase: SupabaseClient<any, "public", any>, userId: string): Promise<Player[]> => {
+  const { data: playerIds } = await supabase.from("userplayers").select("playerid").eq("uuid", userId);
+
+  // get player objects from playerIds
+  if (!playerIds || playerIds.length === 0) {
+    return [];
+  }
+  const { data: players } = await supabase.from("players").select("*").in("playerid", playerIds.map(player => player.playerid));
+
+  // convert players to an array of Player objects
+  if (!players || players.length === 0) {
+    return [];
+  }
+  const playersArray = players.map(player => {
+    return {
+      playerid: player.playerid,
+      name: player.name,
+      price: player.price,
+      squad: player.squad
+    }
+  });
+
+
+  return playersArray;
+}
+
+const getTeams = async (supabase: SupabaseClient<any, "public", any>, userId: string): Promise<[Team, Team[]]> => {
+  const { data: teams } = await supabase.from("users").select("fullname, teamname, total")
+  const { data: userTeam } = await supabase.from("users").select("fullname, teamname, total").eq("id", userId);
+
+  const userTeamObj = userTeam!.map(team => {
+    return {
+      fullname: team.fullname,
+      teamname: team.teamname,
+      total: team.total
+    }
+  });
+
+  const teamsArray = teams!.map(team => {
+    return {
+      fullname: team.fullname,
+      teamname: team.teamname,
+      total: team.total
+    }
+  });
+
+  // should only have one user team
+  return [userTeamObj[0], teamsArray];
+}
+
+ const getSwaps = async (supabase: SupabaseClient<any, "public", any>, userId: string): Promise<Swap[]> => {
+  const { data: swaps } = await supabase.from("swaps").select("oldplayerid, newplayerid").eq("uuid", userId);
+
+  // convert swaps to an array of Swap objects
+  if (!swaps || swaps.length === 0) {
+    return [];
+  }
+
+  const swapsArray = swaps.map(swap => {
+    return {
+      oldplayerid: swap.oldplayerid,
+      newplayerid: swap.newplayerid
+    }
+  });
+
+  return swapsArray;
+ } 
+
+export default async function Home() {
+  
+  const { players, userTeamInfo, teams, swaps, currentGWPoints } = await getSupabaseInfo();
+
   return (
-    <div className="w-full">
-      <div className="flex-1 w-full h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-red-950">
-        <h1>Home Page</h1>
-      </div>
+    <div>
+      <h1>Home</h1>
+      <TeamDisplay userTeamInfo={userTeamInfo} players={players} swaps={swaps} />
+      <Leaderboard teams={teams} />
     </div>
   );
-};
 
-export default HomePage;
+  
+
+
+
+
+}
