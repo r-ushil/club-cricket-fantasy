722b7b97f94c0cf8d63a3080b1ebaaa3e11fc2ea
Rushil Patel <rushil.patel791@gmail.com>
2024-04-22 19:26:10 +0100

    feat: skeleton leaderboard component

---
+++ components/Leaderboard.tsx
@@ -0,0 +1,16 @@
+import { Team } from "@/types/team";
+import { Swap } from "@/types/swap";
+import { Player } from "@/types/player";
+
+interface LeaderboardProps {
+    teams: Team[];
+}
+
+
+export default function TeamDisplay({ teams }: LeaderboardProps) {
+    return (
+        <div>
+            Todo
+        </div>
+    )
+}
