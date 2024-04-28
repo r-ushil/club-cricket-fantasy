d9bd77f4c110c45f0c9a9c7a6b22841e557caa01
Rushil Patel <rushil.patel791@gmail.com>
2024-04-28 23:19:30 +0100

    fix: responsive navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,10 +2,10 @@ import AuthButton from "./AuthButton";
 
 export default function NavBar() {
   return (
-    <nav className="w-full flex items-center justify-between bg-gray-800 border-b border-b-foreground/10 h-16 py-4 px-10">
-      <div className="flex items-center">
-        <img src="/icucc_logo.png" alt="ICUCC Logo" className="h-12" />
-        <h3 className="text-white text-xl font-bold pl-4">ICUCC Fantasy</h3>
+    <nav className="w-full flex items-center justify-between bg-gray-800 border-b border-b-foreground/10 h-16 py-2 px-4 sm:py-4 sm:px-10">
+      <div className="flex items-center space-x-2 sm:space-x-4">
+        <img src="/icucc_logo.png" alt="ICUCC Logo" className="h-8 sm:h-12" />
+        <h3 className="text-white text-base sm:text-xl font-bold">ICUCC Fantasy</h3>
       </div>
       <AuthButton />
     </nav>
