0d47e9bdfedb2bb98bb8b23fc6e570dcddc9ec42
Rushil Patel <rushil.patel791@gmail.com>
2024-04-28 21:20:30 +0100

    fix: shirt team colour

--- components/TshirtIcon.tsx
+++ components/TshirtIcon.tsx
@@ -11,7 +11,7 @@ const TshirtIcon = ({ shirtText, selected}: TshirtIconProps) => {
     <div className="relative">
       <TbShirtFilled className={`text-${shirtColour} text-6xl`} />
       <span
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-md font-bold flex items-center justify-center"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-md font-bold flex items-center justify-center"
       >{shirtText}
       </span>
     </div>
