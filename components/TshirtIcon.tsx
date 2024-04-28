a9cd56ac774b4f602da6488b2ab897b38a5aa1f7
Rushil Patel <rushil.patel791@gmail.com>
2024-04-28 23:03:38 +0100

    fix: tshirt text white

--- components/TshirtIcon.tsx
+++ components/TshirtIcon.tsx
@@ -10,7 +10,7 @@ const TshirtIcon = ({ shirtText, selected}: TshirtIconProps) => {
     <div className="relative">
       <TbShirtFilled className={`text-6xl ${selected ? 'text-blue-950' : 'text-gray-800'}`} />
       <span
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-md font-bold flex items-center justify-center"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-md font-bold flex items-center justify-center"
       >{shirtText}
       </span>
     </div>
