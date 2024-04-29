4791100d68821268c0ae42c59265aa2b92683162
sa2021 <sa2021@ic.ac.uk>
2024-04-29 22:57:42 +0100

    feat: add background for mobile home FAQs

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { BsChevronCompactDown } from "react-icons/bs";
 
 const checkAuth = async () => {
   const supabase = createClient();
-  
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -27,8 +27,8 @@ export default async function Index() {
     { "title": "How do I get started?", "content": "Sign up, select players to make a team" },
     { "title": "What are the restrictions on making my team?", "content": "Budget. 4 per squad." },
     { "title": "How are points calculated?", "content": "rules" },
-    { "title": "What do I do if I find a bug?", "content": "report it"},
-    { "title": "Should I bet against the banker?", "content": "Never"}
+    { "title": "What do I do if I find a bug?", "content": "report it" },
+    { "title": "Should I bet against the banker?", "content": "Never" }
   ]
   return (
     <>
@@ -48,7 +48,7 @@ export default async function Index() {
         {/* Portrait banner for mobile screens */}
         <div className="lg:hidden w-full h-full">
           <Image
-            src="/banner_mobile.png" 
+            src="/banner_mobile.png"
             alt="Portrait Banner"
             layout="fill"
             objectFit="cover"
@@ -73,23 +73,26 @@ export default async function Index() {
 
       {/* Content below the banner */}
       <div className="flex flex-col w-full">
-        <div className="container mx-auto px-4 mt-8">
-          <h2 className="text-3xl font-bold py-4 my-4">Frequently Asked Questions</h2>
-          <FAQs items={FAQsData} />
+        <div className="lg:h-screen lg:bg-cover lg:bg-[url('/home_background.png')] bg-ic">
+          <div className="w-full flex flex-col items-center lg:pt-10">
+            <h2 className="text-3xl text-gray-200 text-center font-bold py-4 my-4">Frequently Asked Questions</h2>
+            <FAQs items={FAQsData}></FAQs>
+          </div>
+
+          {/* Mobile graphic */}
+          <div className="lg:hidden flex w-screen">
+            <Image
+              src="/homefaq_mobile.png"
+              alt="Home background"
+              width={1080}
+              height={585}
+              quality={100}
+            />
+          </div>
         </div>
 
-        <footer className="w-full bg-black border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
+        <footer className="w-full bg-black border-t border-t-foreground/10 p-6 flex justify-center text-center text-sm">
+          <p>Powered by <span className="font-semibold">Mark Deane</span></p>
         </footer>
       </div>
     </>
