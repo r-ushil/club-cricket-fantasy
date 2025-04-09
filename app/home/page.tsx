346177a1aacb19162d4561ddfb03289cf79a5a39
sa2021 <sa2021@ic.ac.uk>
2025-04-09 15:42:59 +0100

    feat: update team display UI

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,6 +11,8 @@ import FAQs from "@/components/FAQs";
 import Image from "next/image";
 import Announcement from "@/components/Announcement";
 import Footer from "@/components/Footer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import HomeTabs from "@/components/HomeTabs";
 
 interface userTeamInfo {
   teamInfo: Team;
@@ -37,16 +39,16 @@ const getSupabaseInfo = async () => {
       captainName: string;
       newCaptainName: string | null;
     }];
-  
+
     const userQueries: Promise<UserQueriesResult> = Promise.all([
-        getTeamPlayers(supabase, user.id),
-        getSwaps(supabase, user.id),
-        getTeams(supabase, user.id),
-        getCaptainName(supabase, user.id)
+      getTeamPlayers(supabase, user.id),
+      getSwaps(supabase, user.id),
+      getTeams(supabase, user.id),
+      getCaptainName(supabase, user.id)
     ]);
-  
+
     const [players, swaps, teams, captainData] = await userQueries as UserQueriesResult;
-  
+
     const currentGWPoints = await getCurrentGWPoints(supabase, players, captainData.captainName);
 
     const userTeamInfo: userTeamInfo = {
@@ -129,11 +131,11 @@ const getTeams = async (supabase: SupabaseClient<any, "public", any>, userId: st
   return teamsArray;
 }
 
-const getCaptainName = async (supabase: SupabaseClient<any, "public", any>, userId: string): Promise<{captainName: string, newCaptainName: string | null}> => {
+const getCaptainName = async (supabase: SupabaseClient<any, "public", any>, userId: string): Promise<{ captainName: string, newCaptainName: string | null }> => {
   const { data: captainId } = await supabase.from("userplayers").select("playerid").eq("uuid", userId).eq("captain", true).single();
 
   if (!captainId) {
-    return {captainName: "", newCaptainName: null};
+    return { captainName: "", newCaptainName: null };
   }
 
   const { data: captainNameObj } = await supabase.from("players").select("name").eq("playerid", captainId.playerid).single();
@@ -142,7 +144,7 @@ const getCaptainName = async (supabase: SupabaseClient<any, "public", any>, user
   const { data: newCaptainNameObj } = await supabase.from("players").select("name").eq("playerid", newCaptainId!.nextcapt).single();
 
   if (!newCaptainNameObj) {
-    return { captainName: captainNameObj!.name , newCaptainName: null};
+    return { captainName: captainNameObj!.name, newCaptainName: null };
   }
 
   return { captainName: captainNameObj!.name, newCaptainName: newCaptainNameObj.name };
@@ -196,9 +198,8 @@ export default async function Home() {
       <NavBar />
       <Announcement />
       <div className="min-h-screen">
-        <div className="bg-gradient-to-b from-blue-950 to-gray-900 grid lg:grid-cols-2 grid-cols-1 gap-8 px-4 pt-12 pb-10 lg:gap-16 lg:px-16 lg:pt-12 lg:pb-10">
-          <TeamDisplay userTeamInfo={userTeamInfo} />
-          <Leaderboard teams={teams} />
+        <div className="flex bg-gradient-to-b from-blue-950 to-gray-900 px-4 pt-12 pb-10 lg:px-16 lg:pt-12 lg:pb-10">
+          <HomeTabs userTeamInfo={userTeamInfo} teams={teams} />
         </div>
 
         <div className="lg:h-screen lg:bg-cover lg:bg-bottom lg:bg-[url('/home_background.png')] bg-ic">
